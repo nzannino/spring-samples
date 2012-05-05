621e8b1e207efd9a7938068945f4953f374e38ac
Nicola Zannino <n.zannino@gmail.com>
2012-05-05 15:13:49 +0200

    first commit

---
+++ hornetQ/src/main/java/com/nzannino/hornetq/HomeController.java
@@ -0,0 +1,69 @@
+package com.nzannino.hornetq;
+
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Autowired
+	private JmsTemplate jmsTemplate;
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! the client locale is "+ locale.toString());
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		int counter = 1;
+		while(counter<100){
+			
+			jmsTemplate.send(new MessageCreator() {				
+				@Override
+	            public Message createMessage(Session session) throws JMSException {
+					String msg = "[msg @ "+new Timestamp(System.currentTimeMillis())+" ]";
+	                TextMessage message = session.createTextMessage(msg);	                
+	                logger.info("Sending message: " + msg);	                
+	                return message;
+	            }
+	        });
+			
+			counter++;
+		}
+		
+		
+		
+		return "home";
+	}
+	
+}
---
+++ hornetQ/src/main/java/com/nzannino/hornetq/jms/HornetListener.java
@@ -0,0 +1,24 @@
+package com.nzannino.hornetq.jms;
+
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+public class HornetListener implements MessageListener{
+
+	@Override
+	public void onMessage(Message message) {
+		// TODO Auto-generated method stub
+		if (message instanceof TextMessage) {
+		      try {
+		        System.out.println("Received message: "+((TextMessage) message).getText());
+		        Thread.sleep(60000);
+		      } catch (Exception ex) {
+		        throw new RuntimeException(ex);
+		      }
+		    } else {
+		      throw new IllegalArgumentException("Message must be of type TextMessage");
+		    }
+	}
+
+}
